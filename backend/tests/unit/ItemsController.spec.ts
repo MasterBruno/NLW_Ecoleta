957d0658fe503791bcad6489bb7941a331a55193
masterbruno <brunoc.15@hotmail.com>
2020-06-07 13:09:02 -0300

    [nlw_ecoleta] Setup inicial backend.

---
+++ backend/tests/unit/ItemsController.spec.ts
@@ -0,0 +1,7 @@
+import ItemsController from "../../src/controllers/ItemsController";
+
+describe("ItemsController", () => {
+  it("Controller criado", () => {
+    expect(ItemsController).toBeTruthy();
+  });
+});
